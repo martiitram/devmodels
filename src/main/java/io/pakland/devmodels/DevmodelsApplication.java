270aa45b3d4fa47ee8d80c3d9402364f51aecfba
Marti Rosell <martirosellolle@gmail.com>
2023-01-24 20:24:06 +0100

    first comit

---
+++ src/main/java/io/pakland/devmodels/DevmodelsApplication.java
@@ -0,0 +1,13 @@
+package io.pakland.devmodels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevmodelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevmodelsApplication.class, args);
+	}
+
+}
