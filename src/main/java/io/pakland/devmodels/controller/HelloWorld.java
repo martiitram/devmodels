4cce46bbfdb5be40f051f4efc2e06e70131a8ada
Paco Lozano <plozano@pakland.io>
2023-01-24 21:23:19 +0100

    feat: add HelloWorld controller

---
+++ src/main/java/io/pakland/devmodels/controller/HelloWorld.java
@@ -0,0 +1,15 @@
+package io.pakland.devmodels.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping("hello")
+    String printHelloWorld() {
+        return "hola";
+    }
+}
